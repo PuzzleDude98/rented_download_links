224be0f75b2ff8ceefabca11b90ad8237f0136fd
PuzzleDude98 <48701078+PuzzleDude98@users.noreply.github.com>
2022-01-18 11:49:17 -0800

    Update ZoomsTemplate.bat

--- ZoomsTemplate.bat
+++ ZoomsTemplate.bat
@@ -50,3 +50,4 @@ goto start
 ::To add other more zooms just copy pase the paragraph above and change it to whatever you want.
 ::I'm sure you already could figure that out though
 :: BTW it does not have to be labeled(the :p{number} at the start of each paragraph) as a number, it can be :teachers or :gaming or whatever, not just :p8
+::Also you can change the color by changing the suber where it says COLOR 07 up top. The first digit is the background color and the second is the text color.
